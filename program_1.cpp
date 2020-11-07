ba0e0438878981f90cf6ca94b0a257cb74a92c85
Manish <techwork7236@gmail.com>
2020-11-07 15:14:29 +0530

    first Program

--- program_1.cpp
+++ program_1.cpp
@@ -1,3 +1,15 @@
+#include<iostream>
+using namespace std;
 int main(){
-    cout<<"To kaise hai aap log"
+    int t,n ;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>n;
+        int sum=0;
+        while(n!=0){
+            sum+=n%10;
+            n=n/10;
+        }
+        cout<<sum<<endl;
+    }
 }
